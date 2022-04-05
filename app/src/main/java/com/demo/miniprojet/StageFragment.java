477c137977ee0f17ca3c5e097f5c9745e21a217c
mazenmdimegh <mazenmdimagh@gmail.com>
2022-04-05 12:10:53 +0100

    Stage Page

--- app/src/main/java/com/demo/miniprojet/StageFragment.java
+++ app/src/main/java/com/demo/miniprojet/StageFragment.java
@@ -1,31 +1,30 @@
 package com.demo.miniprojet;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfileFragment extends Fragment {
+public class StageFragment extends Fragment {
     private RecyclerView recyclerView ;
     String s1[], s2[];
     int images[] = {R.drawable.flateo,R.drawable.flateo,R.drawable.flateo,R.drawable.flateo,R.drawable.flateo};
 
-    public ProfileFragment() {
+    public StageFragment() {
         // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_stage, container, false);
         recyclerView = view.findViewById(R.id.recycler_view);
         s1 = getResources ().getStringArray(R.array.titres);
         s2 = getResources ().getStringArray (R.array.description);recyclerView.setHasFixedSize(true);
