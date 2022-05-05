5eb5032272336bcd2707734b5b19a921c1c0b778
mazenmdimegh <mazenmdimagh@gmail.com>
2022-05-05 11:58:38 +0100

    Entreprise Page

--- app/src/main/java/com/demo/miniprojet/EntrepriseFragment.java
+++ app/src/main/java/com/demo/miniprojet/EntrepriseFragment.java
@@ -6,36 +6,35 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 /**
  * A simple {@link Fragment} subclass.
  */
-public class StageFragment extends Fragment {
+public class EntrepriseFragment extends Fragment {
     private RecyclerView recyclerView ;
-    String s1[], s2[];
+    String s1[];
     int images[] = {R.drawable.flateo,R.drawable.germitec,R.drawable.medinbio,R.drawable.dipole,R.drawable.iwasecosfa};
 
-    public StageFragment() {
+    public EntrepriseFragment() {
         // Required empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_stage, container, false);
+        View view = inflater.inflate(R.layout.fragment_entreprise, container, false);
         recyclerView = view.findViewById(R.id.recycler_view);
-        s1 = getResources ().getStringArray(R.array.titres);
-        s2 = getResources ().getStringArray (R.array.description);recyclerView.setHasFixedSize(true);
+        s1 = getResources ().getStringArray(R.array.Entreprises);
 
         //MyAdapter myAdapter = new MyAdapter(  this, s1, s2, images);
-       //recyclerView.setLayoutManager(new LinearLayoutManager());
-       // recyclerView.setAdapter(new RandomNumListAdapter(1234));
+        //recyclerView.setLayoutManager(new LinearLayoutManager());
+        // recyclerView.setAdapter(new RandomNumListAdapter(1234));
 
-        MyAdapter myAdapter = new MyAdapter( view.getContext(), s1, s2, images);
+        MyAdapter2 myAdapter = new MyAdapter2( view.getContext(), s1, images);
         recyclerView.setAdapter (myAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager( view.getContext()));
+        recyclerView.setLayoutManager(new GridLayoutManager( view.getContext(),5));
         return view;
     }
 }
